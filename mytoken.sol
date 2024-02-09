2ff9e32d34a046b9a090bc141dd93834bf07aaa0
aditisharma0612 <143523593+aditisharma0612@users.noreply.github.com>
2024-02-09 14:43:44 +0530

    Update mytoken.sol

--- mytoken.sol
+++ mytoken.sol
@@ -1,1 +1,10 @@
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+contract Aditi is ERC20 {
+    constructor() ERC20("DCE", "Aditi")  {
+        _mint(msg.sender,10000*(10**18) );
+    }
+}
